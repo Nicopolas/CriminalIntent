bfb5847b1baf17d0e6ab5c51ea083929a96a1215
Zaharov Nikolay <zaharovkoly@mail.ru>
2018-03-21 17:02:34 +0300

    Initial commit

---
+++ app/src/main/java/criminalintent/android/bignerdranch/com/criminalintent/CrimeActivity.java
@@ -0,0 +1,29 @@
+package criminalintent.android.bignerdranch.com.criminalintent;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class CrimeActivity extends AppCompatActivity {
+    private static final String TAG = "AppCompatActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG,"onCreate called");
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+
+    }
+}
